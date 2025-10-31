c7d131ca6d485a5c53bbbbfac2f47a35144668cc
Jummy-gr <22200224@ue.edu.pe>
2025-10-31 16:53:30 -0500

    Realizando cambios y merando mi codigo

--- UESAN.Ecommerce.CORE/Infrastructure/Data/StoreDbContext.cs
+++ UESAN.Ecommerce.CORE/Infrastructure/Data/StoreDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using UESAN.Ecommerce.CORE.Core.Entities;
 
 namespace UESAN.Ecommerce.CORE.Infrastructure.Data;
 
@@ -32,7 +31,10 @@ public partial class StoreDbContext : DbContext
 
     public virtual DbSet<User> User { get; set; }
 
-  
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-OGO0EOFS\\NANA;Database=StoreDB;Integrated Security=True;TrustServerCertificate=True");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>(entity =>
